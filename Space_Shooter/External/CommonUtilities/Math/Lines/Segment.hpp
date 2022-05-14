8eae382add296edbc5b85a634a52790622a79fc1
Henrik Jangefelt <henke9105@hotmail.com>
2022-05-14 02:20:02 +0200

    Skeleton class for (line) Segment

---
+++ Space_Shooter/External/CommonUtilities/Math/Lines/Segment.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+/* - Description: a Segment is defined by a start point and an end point. - */
+
+namespace CommonUtilities
+{
+	template <typename T>
+	class Segment
+	{
+	public:
+		Segment();
+
+	private:
+
+	};
+
+#pragma region DEFINITIONS
+
+	template <typename T>
+	Segment<T>::Segment()
+	{
+	}
+
+#pragma endregion DEFINITIONS
+}
